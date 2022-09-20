83524f26fa342ab30741356890bdd1c7dcffbe02
Myke William <maike.study@gmail.com>
2022-09-20 17:54:06 -0300

    addicionando validações nos campos de input cadastro

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,13 @@
 const express = require ('express')
 const router = express.Router()
 
+// ==================== middlweares is required ==================== //
 const user = require('../controllers/usuariosController.js')
+const ctValidator = require('../middlewares/ctValidator')
 
+// ==================== defined routes ==================== //
 router.get('/', user.index)
-router.post('/cadastro', user.cadastro)
+router.post('/cadastro',ctValidator, user.cadastro)
 router.post('/login', user.login)
 
 module.exports = router
